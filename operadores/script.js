0ca6e9094ff0fbc8fe3f62526012a4cd33c21deb
Matheus Farias <matheusaf.contato@gmail.com>
2025-03-17 09:58:59 -0300

    correção modulo %

--- operadores/script.js
+++ operadores/script.js
@@ -12,7 +12,7 @@ console.log(`3 + 4 = ${3 + 4}`);
 console.log(`3 - 4 = ${3 - 4}`);
 console.log(`3 * 4 = ${3 * 4}`);
 console.log(`19 / 3 = ${19 / 5}`);
-console.log(`19 % 3 = ${19 % 5}`);
+console.log(`19 % 5 = ${19 % 5}`);
 console.log(`3 ** 4 = ${3 ** 4}`);
 /*
 Operadores comparativos
